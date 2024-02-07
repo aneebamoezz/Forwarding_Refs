2595f459b8640ab8b54edd9a725ae77772a85ac4
aneebamoezz <anibakaleem@gmail.com>
2024-02-07 05:00:07 +0500

    commit initial files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FRParentInput from './components/FRParentInput';
+import Title from './components/Title';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <FRParentInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div>
+        <h2>Forwarding Ref</h2>
+    </div>
+  )
+}
+
+export default Title
